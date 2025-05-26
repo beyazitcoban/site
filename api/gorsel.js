3c91402d598744195694386eb4fa811d93f04bb4
beyazitcoban <cobanbestami@gmail.com>
2025-05-26 12:17:35 +0300

    Update gorsel.js

--- api/gorsel.js
+++ api/gorsel.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
 
   const redirectUrl = isMobile
     ? 'https://i.hizliresim.com/1a3qg0y.png'      // Mobil için gösterilecek resim
-    : 'hhttps://i.hizliresim.com/39ggk8e.jpg';  // Masaüstü için gösterilecek resim
+    : 'https://i.hizliresim.com/39ggk8e.jpg';  // Masaüstü için gösterilecek resim
 
   res.writeHead(302, { Location: redirectUrl });
   res.end();
